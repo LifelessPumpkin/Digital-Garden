c0f0cfd6d411ac97824a09605bfb935ec7032bad
Logan Harmon <l3harmon@hotmail.com>
2025-05-06 13:33:43 -0400

    Quartz sync: May 6, 2025, 1:33 PM

--- quartz/components/pages/FolderContent.tsx
+++ quartz/components/pages/FolderContent.tsx
@@ -19,8 +19,8 @@ interface FolderContentOptions {
 }
 
 const defaultOptions: FolderContentOptions = {
-  showFolderCount: true,
-  showSubfolders: true,
+  showFolderCount: false,
+  showSubfolders: false,
 }
 
 export default ((opts?: Partial<FolderContentOptions>) => {
